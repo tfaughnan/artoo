135857b21d506b65c0274705e1d4bc4357ddbb3f
Thomas Faughnan <tom@tjf.sh>
2022-10-04 15:01:57 -0400

    style: add remaining named colors and styles

--- style/style.go
+++ style/style.go
@@ -1,8 +1,33 @@
 package style
 
+// https://modern.ircdocs.horse/formatting.html
+
 var (
-	Reset   = "\x0f"
-	Italics = "\x1d"
-	Color   = "\x03"
-	Grey    = "14"
+	Reset         = "\x0f"
+	Bold          = "\x02"
+	Italics       = "\x1d"
+	Underline     = "\x1f"
+	Strikethrough = "\x1e"
+	Monospace     = "\x11"
+	Color         = "\x03"
+	HexColor      = "\x04"
+	ReverseColor  = "\x16"
+
+	White        = "00"
+	Black        = "01"
+	Blue         = "02"
+	Green        = "03"
+	Red          = "04"
+	Brown        = "05"
+	Magenta      = "06"
+	Orange       = "07"
+	Yellow       = "08"
+	LightGreen   = "09"
+	Cyan         = "10"
+	LightCyan    = "11"
+	LightBlue    = "12"
+	Pink         = "13"
+	Grey         = "14"
+	LightGrey    = "15"
+	DefaultColor = "99"
 )
